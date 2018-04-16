d33dfa9fb466ac244071d03511934782f80d895a
carmona44 <daniel_95ca@hotmail.com>
2018-04-16 13:44:02 +0200

    Apariencia listado de los equipos

--- www/js/app.js
+++ www/js/app.js
@@ -31,9 +31,14 @@ function principalController($scope, $ionicPopup) {
     nivel: 'Nivel 1',
     esPortero: false
   };
-
   $scope.jugador = {};
   $scope.jugadores = [];
+  $scope.porteros = [];
+  $scope.nivel1 = [];
+  $scope.nivel2 = [];
+  $scope.nivel3 = [];
+  $scope.equipoA = [];
+  $scope.equipoB = [];
 
   $scope.nuevoJugador = function () {
     if($scope.data.nombre != ''){
@@ -43,6 +48,7 @@ function principalController($scope, $ionicPopup) {
         esPortero: $scope.data.esPortero
       }
       $scope.jugadores.push($scope.jugador);
+
       $scope.data.nombre = '';
       $scope.data.esPortero = false;
     }
@@ -53,5 +59,36 @@ function principalController($scope, $ionicPopup) {
       title: 'AYUDA',
       template: 'Descripcion de la ayuda'
     });
+  }
+
+  $scope.hacerEquipos = function () {
+    $scope.porteros = [];
+    $scope.nivel1 = [];
+    $scope.nivel2 = [];
+    $scope.nivel3 = [];
+
+    if ($scope.jugadores.length > 0){
+      for (var i=0; i<$scope.jugadores.length; i++){
+        $scope.jugador = $scope.jugadores[i];
+        if($scope.jugador.esPortero){
+          $scope.porteros.push($scope.jugador);
+        } else {
+          if($scope.jugador.nivel == 'Nivel 1'){
+            $scope.nivel1.push($scope.jugador);
+          } else if ($scope.jugador.nivel == 'Nivel 2'){
+            $scope.nivel2.push($scope.jugador);
+          } else {
+            $scope.nivel3.push($scope.jugador);
+          }
+        }
+      }
+    }
+
+    //Math.floor(Math.random() * 2)
+
+    console.log($scope.porteros);
+    console.log($scope.nivel2);
+    console.log($scope.nivel1);
+    console.log($scope.nivel3);
   }
 }
